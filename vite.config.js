81fe54247f695c0eba1b3ec29c3ea51f7394232c
Butord2025 <butordv@gmail.com>
2025-08-06 13:00:16 +0300

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/handyhelper/', // 🔹 має бути тут, на верхньому рівні
   server: {
-	base: '/handyhelper/', // 🔹 назва твого репозиторію
     host: '0.0.0.0',
     port: 3000,
     allowedHosts: ['.daytona.work'],
     strictPort: true
   }
-});
+})
