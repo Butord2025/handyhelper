b62973bdb6c0cc6091b7963125bc234d4d160aeb
Butord2025 <butordv@gmail.com>
2025-08-06 12:54:15 +0300

    Fix GitHub Pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+	base: '/handyhelper/', // 🔹 назва твого репозиторію
     host: '0.0.0.0',
     port: 3000,
     allowedHosts: ['.daytona.work'],
